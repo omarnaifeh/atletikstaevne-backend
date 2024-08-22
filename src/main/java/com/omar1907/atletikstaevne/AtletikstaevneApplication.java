a052d7f41a121b0e80ca5774305c0c8e8c8bf264
omarnaifeh <omar1907@stud.kea.dk>
2024-08-22 14:34:43 +0200

    Initial commit

---
+++ src/main/java/com/omar1907/atletikstaevne/AtletikstaevneApplication.java
@@ -0,0 +1,13 @@
+package com.omar1907.atletikstaevne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtletikstaevneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtletikstaevneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omar1907/atletikstaevne/AtletikstaevneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar1907.atletikstaevne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtletikstaevneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
